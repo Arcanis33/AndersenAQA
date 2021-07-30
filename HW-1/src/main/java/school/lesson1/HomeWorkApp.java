ea234b2fd82df15c20bea352009817e94871b1ae
Arcanis33 <artem.gurenko@gmail.com>
2021-07-30 12:10:31 +0200

    Commit30.07.2021 01:31

---
+++ HW-1/src/main/java/school/lesson1/HomeWorkApp.java
@@ -0,0 +1,15 @@
+package school.lesson1;
+
+public class HomeWorkApp {
+ public static void main(String[] args) {
+  printThreeWords();
+ }
+
+static void printThreeWords(){
+  System.out.println("Orange");
+  System.out.println("Banana");
+  System.out.println("Apple");
+
+  }
+
+ }
