78f324d98a29ad3997377cc8f9e2e8716338b0bb
Arcanis33 <artem.gurenko@gmail.com>
2021-07-30 18:23:51 +0200

    Commit 30.07 13:01

--- HW-1/src/main/java/school/lesson1/HomeWorkApp.java
+++ HW-1/src/main/java/school/lesson1/HomeWorkApp.java
@@ -1,48 +1,51 @@
 package school.lesson1;
 
 public class HomeWorkApp {
- public static void main(String[] args) {
-  printThreeWords();
-  checkSumSign();
-  printColor();
-  compareNumbers();
- }
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
 
-  static void printThreeWords() {
-    System.out.println("Orange");
-    System.out.println("Banana");
-    System.out.println("Apple");
-}
-  static void checkSumSign(){
-      int first = 10;
-      int second = 2;
-      int third = (first + second);
-      if (third >=0) {
-          System.out.println("Сумма положительная и равняется " + third);
-      } else {
-          System.out.println("Сумма отрицательная и равняется " + third);
-      }
+    static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    static void checkSumSign() {
+        int first = 10;
+        int second = 2;
+        int third = first + second;
+        if (third >= 0) {
+            System.out.println("Сумма положительная и равняется " + third);
+        } else {
+            System.out.println("Сумма отрицательная и равняется " + third);
+        }
 
     }
-  static void printColor(){
-     int value = 101;
-     if(value <= 0) {
-         System.out.println("Красный");
-     } else if (value <= 100) {
-         System.out.println("Желтый");
-     } else {
-         System.out.println("Зеленый");
-      }
+
+    static void printColor() {
+        int value = 101;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
 
 
-  }
-  static void compareNumbers() {
-      int a = 50;
-      int b = 2;
-      if (a >= b) {
-          System.out.println("a >= b");
-      } else {
-          System.out.println("a < b");
-      }
-  }
+    }
+
+    static void compareNumbers() {
+        int a = 50;
+        int b = 2;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
 }
